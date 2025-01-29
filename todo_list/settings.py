4d20063b85e6d17c6d424cbad844265c3b1cb586
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-01-29 10:58:06 +0200

    update readme requirements and settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hmnp5#9!lhy*#zu2%p9r2$aen($)n9_#1!#^l$h&53-r))^e4c"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
